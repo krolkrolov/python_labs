635753bfd0e421a55d8dcdd763ec420053fa8cfb
Egor Nikolaev <egorka.nikolaev@gmail.com>
2025-09-28 01:37:12 +0300

    правки

---
+++ src/lab01/ex6.py
@@ -0,0 +1,16 @@
+n = int(input().strip())
+
+count_offline = 0
+count_online = 0 
+
+for _ in range(n):
+    data = input().split()
+    
+    format_str = data[-1]
+    
+    if format_str == "True":
+        count_offline += 1
+    else:
+        count_online += 1
+
+print(f"{count_offline} {count_online}")
